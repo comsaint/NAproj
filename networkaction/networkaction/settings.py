abdd49c1b72c00514372d4482880cba649c799b8
Long <longpun88@gmail.com>
2015-02-20 09:58:47 +1100

    Removed South

--- networkaction/networkaction/settings.py
+++ networkaction/networkaction/settings.py
@@ -52,8 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #db migrations
-    'south',
+
     #allauth
     # The Django sites framework is required
     'django.contrib.sites',
